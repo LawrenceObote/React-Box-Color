56c648fa4887920e5a7570b6bbac5e8f33ee205f
Lawrence Obote <dev.obote@gmail.com>
2020-03-27 23:33:18 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      color: 'blue',
+      clickCounter: 0
+    }
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor() {
+    const newColor = this.state.color == 'blue' ? 'orange' : 'blue';
+    this.setState({ color: newColor });
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  onChange = (e) => {
+    this.setState({
+      color: 'orange',
+      clickCounter: this.state.clickCounter++
+    })
+    console.log("click counter ===>", this.state.clickCounter)
+  }
+  render() {
+    return (
+      //parent container
+      <div className="App">
+        <h1>Box color change</h1>
+        <div className="colorBox" 
+             style={{backgroundColor: 
+             this.state.color}}
+             onClick={this.changeColor}
+        >
+          <p>Click Me</p>
+        </div>
+      </div>
+    )
+  }
+}
